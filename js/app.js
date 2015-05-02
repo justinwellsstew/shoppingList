9f1cde38ffb960b4fb6e19fbb0a3a827e8f13b1a
Justin Stewart <stewartjustin3@gmail.com>
2015-05-02 17:38:08 -0400

    old files

--- js/app.js
+++ js/app.js
@@ -1,24 +1,37 @@
 $(document).ready(function(){
-  $('#add-item').click(function(){  
+  $('#add-item').click(function(){ 
+     var listLength = $('#need  li').length;
+    if(listLength > -1){
+      $('#need p').hide();
+    }; 
     var item = $('input').val();
     $('#need >ol').append('<li>' + item + '</li>');
+  
   });
 
   $('ol').on('click', 'li', function(){
+
+    //check whether items are in cart
+    var cartLength = $('#incart  li').length;
+    
+    //hide message if items are in Cart
+    if(cartLength > -1){
+      $('#incart p').hide();
+    };
+
+    //add item to variable
     var shoppingListItem = $(this).text();
+
+    //add item to cart
     $('#incart >ol').append('<li>' + shoppingListItem + '</li>');
+
+    //remove item from shopping list to incart list
     $(this).remove();
   });
 
+  //clear shopping cart
   $('#clear-cart').click(function(){
     $('#incart >ol').children().remove();
   });
-  var listLength = $('#need > ol').children().length;
-  if(listLength < 1){
-    $('#need').append('<p>Add some items to you list</p>');
-  };
-  var cartLength = $('#need > ol').children().length;
-  if(cartLength < 1){
-    $('#incart').append('<p>Your cart is currently empty</p>');
-  };
+  
 });  
